efe906a12450c52eed1b33132f6d8cbc3eb937cb
Faisaly <faisalysalim2808@gmail.com>
2025-08-28 10:29:06 +0300

    database changes

--- uzurisafari/settings.py
+++ uzurisafari/settings.py
@@ -152,22 +152,22 @@ WSGI_APPLICATION = 'uzurisafari.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'uzuridatabase',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Fasilium@2025',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'uzuridatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'Fasilium@2025',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL')
+#     )
+# }
 
 
 # Password validation
